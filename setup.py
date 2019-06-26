ee59ed4ea5b01cbdce81c16b3cc487e2a33fb1a1
Wei-ju Wu <weiju.wu@gmail.com>
2019-06-26 10:13:51 -0700

    integrated mirvestigator

---
+++ setup.py
@@ -0,0 +1,50 @@
+import os
+import re
+import codecs
+from setuptools import setup
+
+NAME = 'firm'
+PACKAGES = ['firm']
+DESCRIPTION = 'FIRM'
+LICENSE = 'LGPL V3'
+URI = 'https://github.com/baliga-lab/firmtoo'
+AUTHOR = 'Baliga Lab, Institute for Systems Biology'
+VERSION = '1.0.0'
+
+KEYWORDS = ['miRvestigator', 'firm']
+
+# See trove classifiers
+# https://testpypi.python.org/pypi?%3Aaction=list_classifiers
+
+CLASSIFIERS = [
+    "Development Status :: 5 - Production/Stable",
+    "Environment :: Console",
+    "Intended Audience :: Science/Research",
+    "Natural Language :: English",
+    "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Topic :: Scientific/Engineering :: Bio-Informatics",
+    "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+
+
+if __name__ == '__main__':
+    setup(name=NAME, description=DESCRIPTION,
+          license=LICENSE,
+          url=URI,
+          version=VERSION,
+          author=AUTHOR,
+          author_email='wwu@systemsbiology.net',
+          maintainer=AUTHOR,
+          maintainer_email='wwu@systemsbiology.net',
+          keywords=KEYWORDS,
+          packages=PACKAGES,
+          zip_safe=False,
+          classifiers=CLASSIFIERS,
+          scripts=['bin/firm', 'bin/firm-findmotifs', 'bin/firm-results', 'bin/mirvestigator'])
